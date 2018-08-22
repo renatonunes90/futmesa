a75a3b971bb37af0fca33339d85f298833a3c107
RENATO-PC\Renato <renatonunes90@gmail.com>
2018-08-22 12:18:09 -0300

    Correções no build path para apontar para o caminho correto do PHP.

--- Back-End/php/dblib/UnitTest/AllTests.bat
+++ Back-End/php/dblib/UnitTest/AllTests.bat
@@ -1,9 +1,9 @@
 set IgnoreCache=1
 set UnitTest=
-call php C:/php-7.2/phpunit-7.2.7.phar --verbose --log-junit resultsDB.xml .\
+call php D:/php-7.2/phpunit-7.2.7.phar --verbose --log-junit resultsDB.xml .\
 pause
 set UnitTest=1
-call php C:/php-7.2/phpunit-7.2.7.phar --verbose --log-junit resultsXML.xml .\
+call php D:/php-7.2/phpunit-7.2.7.phar --verbose --log-junit resultsXML.xml .\
 pause
 
 
