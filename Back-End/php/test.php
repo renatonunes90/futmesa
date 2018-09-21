0a830a350a78b2dfee3fb3f8619fbc8690b9f7aa
Renato Nunes <renato.nunes@southsystem.com.br>
2018-09-21 14:14:03 -0300

    Script de teste de importação sem phpunit.

--- Back-End/php/test.php
+++ Back-End/php/test.php
@@ -12,46 +12,7 @@ echo "Teste inclusão DBLib:";
 require_once "dblib/dblib.php";
 echo "<br>incluiu";
 
-echo "<br><br>Teste inclusão PHPUnit:<br>";
-echo get_include_path();
-set_include_path( get_include_path() . ":/usr/local/bin" );
-
-use PHPUnit\TextUI\PHPUnit_TextUI_Command;
-
-echo "<br>incluiu";
 echo "<br><br>";
-
-//require "PHPUnit/Autoload.php";
-//use PHPUnit\Framework\PHPUnit_TextUI_Command;
-
-//Prevent PHPUnit from outputing anything
-// ob_start();
-// //exec( "dblib/UnitTest/AllTests.bat" );
-
-//Run PHPUnit and log results to results.xml in junit format
-$command = new PHPUnit_TextUI_Command();
-$command->run(array('phpunit', '--verbose', '--log-junit', 'results.xml', 'dblib/UnitTest/'),
-    true);
-
-$result = ob_get_clean();
-
-
-// Print the output of PHPUnit wherever you want
-print_r($result);
-
-// ob_end_clean();
-
-// $suite = new PHPUnit_Framework_TestSuite();
-// $suite->addTestSuite('dblib/UnitTest/');
-
-// // Shunt output of PHPUnit to a variable
-// ob_start();
-// $runner = new PHPUnit_TextUI_TestRunner();
-// $runner->doRun($suite, [], false);
-// $result = ob_get_clean();
-
-// // Print the output of PHPUnit wherever you want
-// print_r($result);
 
 ob_end_clean();
 ?>
