c23a7cc34913b1476118b2bdf7c940009b3deaa7
Renato Nunes <renato.nunes@southsystem.com.br>
2018-10-09 18:11:17 -0300

    Removendo camadas desnecessárias.

--- Front-End/GWT/src/com/futmesa/client/FutMesa.java
+++ Front-End/GWT/src/com/futmesa/client/FutMesa.java
@@ -1,15 +1,16 @@
 package com.futmesa.client;
 
-import com.futmesa.client.base.AppEntryPoint;
 import com.futmesa.client.base.FilterConfig;
 import com.futmesa.client.module.main.MainModule;
 import com.futmesa.client.windows.main.BaseViewport;
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.RootPanel;
 
 
 /**
  */
-public class FutMesa extends AppEntryPoint
+public class FutMesa implements EntryPoint
 {
 
    private MainModule mainModule;
@@ -22,7 +23,33 @@ public class FutMesa extends AppEntryPoint
       // Não faz nada por enquanto...
    }
 
-   @Override
+   /**
+    * Ponto de entrada da interface. Mostra a tela de login e seta o listener para após o carregamento dos tipos.
+    */
+   public void onModuleLoad()
+   {
+      //AuthWindow.getInstance().checkAuth();
+      initializeViewport();
+   }
+
+   /**
+    * Inicializa a viewport principal da ferramenta.
+    */
+   private void initializeViewport()
+   {
+      // LayoutPanel baseViewport = new LayoutPanel();
+      RootPanel.get().add( BaseViewport.getInstance() );
+      // baseViewport.add( MainViewport.getInstance() );
+      // baseViewport.add( new Label( "TEXTO" ) );
+      // MainViewport.getInstance().setUserLogged( AuthWindow.getInstance().getWebUser().getName() );
+
+      // listener para chamar o update da viewport quando a url é alterada
+      History.addValueChangeHandler( handler -> updateViewport() );
+
+      // chama função que será implementada por cada ponto de entrada
+      onLoad();
+   }
+   
    public void onLoad()
    {
       mainModule = new MainModule();
