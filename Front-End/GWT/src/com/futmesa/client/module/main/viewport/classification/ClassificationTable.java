667204a3c202cc068ec995d9fef94a569f8b9bcb
Renato Nunes <renato.nunes@southsystem.com.br>
2018-10-17 17:05:01 -0300

    Ajustesno layout da tabela.

---
+++ Front-End/GWT/src/com/futmesa/client/module/main/viewport/classification/ClassificationTable.java
@@ -0,0 +1,253 @@
+package com.futmesa.client.module.main.viewport.classification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.futmesa.client.businessinteligence.Classification;
+import com.google.gwt.cell.client.Cell.Context;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.dom.builder.shared.TableCellBuilder;
+import com.google.gwt.dom.builder.shared.TableRowBuilder;
+import com.google.gwt.dom.client.Style.FontWeight;
+import com.google.gwt.dom.client.Style.TextAlign;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.user.cellview.client.AbstractCellTableBuilder;
+import com.google.gwt.user.cellview.client.AbstractHeaderOrFooterBuilder;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Header;
+import com.google.gwt.user.cellview.client.TextHeader;
+
+/**
+ * Viewport com exemplos de utilização do SimpleGrid.
+ */
+public class ClassificationTable {
+
+	/**
+	 * The resources used by this example.
+	 */
+	interface Resources extends ClientBundle {
+
+		/**
+		 * Get the styles used but this example.
+		 */
+		@Source("classificationtable.css")
+		Styles styles();
+	}
+
+	/**
+	 * The CSS Resources used by this example.
+	 */
+	interface Styles extends CssResource {
+
+		String customClassificationHeader();
+
+		String customColumn();
+
+		String customEvenColumn();
+
+		String cellTable();
+	}
+
+	private class CustomHeaderBuilder extends AbstractHeaderOrFooterBuilder<Classification> {
+
+		private final String headerStyle;
+
+		public CustomHeaderBuilder(CellTable<Classification> dataGrid) {
+			super(dataGrid, false);
+			setSortIconStartOfLine(false);
+			headerStyle = resources.styles().customClassificationHeader();
+		}
+
+		@Override
+		protected boolean buildHeaderOrFooterImpl() {
+			// Style style = dataGrid.getResources().style();
+
+			// Add a 2x2 header above the checkbox and show friends columns.
+			TableRowBuilder tr = startRow();
+
+			// Add column headers.
+			tr = startRow();
+
+			buildHeader(tr, "Classificação", true);
+			buildHeader(tr, "P", false);
+			buildHeader(tr, "J", false);
+			buildHeader(tr, "V", false);
+			buildHeader(tr, "E", false);
+			buildHeader(tr, "D", false);
+			buildHeader(tr, "GP", false);
+			buildHeader(tr, "GC", false);
+			buildHeader(tr, "SD", false);
+			buildHeader(tr, "%", false);
+
+			tr.endTR();
+
+			return true;
+		}
+
+		/**
+		 * Renders the header of one column, with the given options.
+		 * 
+		 * @param out    the table row to build into
+		 * @param header the {@link Header} to render
+		 * @param column the column to associate with the header
+		 */
+		private void buildHeader(TableRowBuilder out, String headerStr, boolean isFirst) {
+
+			// Create the table cell.
+			TableCellBuilder th = out.startTH().className(headerStyle);
+			if (isFirst) {
+				th.style().width(240, Unit.PX).textAlign(TextAlign.LEFT).endStyle();
+			}
+
+			Header<String> header = new TextHeader(headerStr);
+
+			// Render the header.
+			Context context = new Context(0, 2, header.getKey());
+			renderHeader(th, context, header);
+
+			// End the table cell.
+			th.endTH();
+		}
+	}
+
+	/**
+	 * Renders the data rows that display each contact in the table.
+	 */
+	private class CustomTableBuilder extends AbstractCellTableBuilder<Classification> {
+
+		private final StringBuilder evenCellStyles;
+		private final String cellStyles;
+		private NumberFormat numberFormatter;
+
+		public CustomTableBuilder(CellTable<Classification> dataGrid) {
+			super(dataGrid);
+
+			// Cache styles for faster access.
+			// Calculate the cell styles.
+			cellStyles = resources.styles().customColumn();
+
+			evenCellStyles = new StringBuilder(resources.styles().customEvenColumn());
+			evenCellStyles.append(" " + resources.styles().customColumn());
+
+			numberFormatter = NumberFormat.getFormat("###.##");
+		}
+
+		@Override
+		public void buildRowImpl(Classification rowValue, int absRowIndex) {
+
+			TableRowBuilder row = startRow();
+
+			// Player
+			TableCellBuilder td = row.startTD();
+			td.className(cellStyles);
+			td.style().textAlign(TextAlign.LEFT).fontSize(17, Unit.PX).endStyle();
+			td.text(String.valueOf(absRowIndex + 1) + " " + rowValue.getPlayerName());
+			td.endTD();
+
+			// Points
+			td = row.startTD();
+			td.className(evenCellStyles.toString());
+			td.style().fontWeight(FontWeight.BOLD).endStyle();
+			td.text(String.valueOf(rowValue.getPoints()));
+			td.endTD();
+
+			// Games
+			td = row.startTD();
+			td.className(cellStyles);
+			td.text(String.valueOf(rowValue.getNumberOfGames()));
+			td.endTD();
+
+			// Wins
+			td = row.startTD();
+			td.className(evenCellStyles.toString());
+			td.text(String.valueOf(rowValue.getWins()));
+			td.endTD();
+
+			// Ties
+			td = row.startTD();
+			td.className(cellStyles);
+			td.text(String.valueOf(rowValue.getTies()));
+			td.endTD();
+
+			// Losses
+			td = row.startTD();
+			td.className(evenCellStyles.toString());
+			td.text(String.valueOf(rowValue.getLosses()));
+			td.endTD();
+
+			// Goals pro
+			td = row.startTD();
+			td.className(cellStyles);
+			td.text(String.valueOf(rowValue.getGoalsPro()));
+			td.endTD();
+
+			// Goals con
+			td = row.startTD();
+			td.className(evenCellStyles.toString());
+			td.text(String.valueOf(rowValue.getGoalsCon()));
+			td.endTD();
+
+			// Goals difference
+			td = row.startTD();
+			td.className(cellStyles);
+			td.text(String.valueOf(rowValue.getGoalsDiff()));
+			td.endTD();
+
+			// Win rate
+			td = row.startTD();
+			td.className(evenCellStyles.toString());
+			td.text(numberFormatter.format(rowValue.getWinRate()));
+			td.endTD();
+
+			row.endTR();
+		}
+	}
+
+	/**
+	 * The main CellTable.
+	 */
+	private CellTable<Classification> cellTable;
+
+	/**
+	 * The resources used by this example.
+	 */
+	private Resources resources;
+
+	/**
+	 * Construtor padrão.
+	 */
+	public ClassificationTable() {
+
+		resources = GWT.create(Resources.class);
+		resources.styles().ensureInjected();
+
+		// Create a CellTable.
+
+		// Set a key provider that provides a unique key for each contact. If key is
+		// used to identify contacts when fields (such as the name and address)
+		// change.
+		cellTable = new CellTable<Classification>(Classification.KEY_PROVIDER);
+		cellTable.setStyleName( resources.styles().cellTable() );
+
+		// Specify a custom table.
+		cellTable.setHeaderBuilder(new CustomHeaderBuilder(cellTable));
+		cellTable.setTableBuilder(new CustomTableBuilder(cellTable));
+	}
+
+   public CellTable<Classification> asWidget()
+   {
+	   return cellTable;
+   }
+
+	public void updateClassification(JsArray<Classification> classifications) {
+		List<Classification> data = new ArrayList<Classification>();
+		for (int i = 0; i < classifications.length(); i++) {
+			data.add(classifications.get(i));
+		}
+		cellTable.setRowData(data);
+	}
+}
--- Front-End/GWT/src/com/futmesa/client/module/main/viewport/classification/ClassificationViewport.java
+++ Front-End/GWT/src/com/futmesa/client/module/main/viewport/classification/ClassificationViewport.java
@@ -1,29 +1,14 @@
 package com.futmesa.client.module.main.viewport.classification;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.futmesa.client.base.ViewportInterface;
 import com.futmesa.client.businessinteligence.Classification;
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
-import com.google.gwt.dom.builder.shared.TableCellBuilder;
-import com.google.gwt.dom.builder.shared.TableRowBuilder;
-import com.google.gwt.dom.client.Style.FontWeight;
-import com.google.gwt.dom.client.Style.TextAlign;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.i18n.client.NumberFormat;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.cellview.client.AbstractCellTableBuilder;
-import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.NoSelectionModel;
 
 /**
  * Viewport com exemplos de utilização do SimpleGrid.
@@ -35,279 +20,30 @@ public class ClassificationViewport implements ViewportInterface {
 	interface ClassificationViewportUiBinder extends UiBinder<HorizontalPanel, ClassificationViewport> {
 	}
 
-	/**
-	 * The resources used by this example.
-	 */
-	interface Resources extends ClientBundle {
-
-		/**
-		 * Get the styles used but this example.
-		 */
-		@Source("classificationviewport.css")
-		Styles styles();
-	}
-
-	/**
-	 * The CSS Resources used by this example.
-	 */
-	interface Styles extends CssResource {
-
-		String classificationTitle();
-		
-		String customColumn();
-
-		String customEvenColumn();
-
-		String cellTable();
-	}
-
-	/**
-	 * Renders the data rows that display each contact in the table.
-	 */
-	private class CustomTableBuilder extends AbstractCellTableBuilder<Classification> {
-
-		private final StringBuilder evenCellStyles;
-		private final String cellStyles;
-		private NumberFormat numberFormatter;
-
-		public CustomTableBuilder(CellTable<Classification> dataGrid) {
-			super(dataGrid);
-
-			// Cache styles for faster access.
-			// Calculate the cell styles.
-			cellStyles = resources.styles().customColumn();
-
-			evenCellStyles = new StringBuilder(resources.styles().customEvenColumn());
-			evenCellStyles.append(" " + resources.styles().customColumn());
-
-			numberFormatter = NumberFormat.getFormat("###.##");
-		}
-
-		@Override
-		public void buildRowImpl(Classification rowValue, int absRowIndex) {
-
-			TableRowBuilder row = startRow();
-
-			// Player
-			TableCellBuilder td = row.startTD();
-			td.className(cellStyles);
-			td.style().textAlign(TextAlign.LEFT).fontSize(17, Unit.PX).endStyle();
-			td.text(String.valueOf(absRowIndex + 1) + " " + rowValue.getPlayerName());
-			td.endTD();
-
-			// Points
-			td = row.startTD();
-			td.className(evenCellStyles.toString());
-			td.style().fontWeight(FontWeight.BOLD).endStyle();
-			td.text(String.valueOf(rowValue.getPoints()));
-			td.endTD();
-
-			// Games
-			td = row.startTD();
-			td.className(cellStyles);
-			td.text(String.valueOf(rowValue.getNumberOfGames()));
-			td.endTD();
-
-			// Wins
-			td = row.startTD();
-			td.className(evenCellStyles.toString());
-			td.text(String.valueOf(rowValue.getWins()));
-			td.endTD();
-
-			// Ties
-			td = row.startTD();
-			td.className(cellStyles);
-			td.text(String.valueOf(rowValue.getTies()));
-			td.endTD();
-
-			// Losses
-			td = row.startTD();
-			td.className(evenCellStyles.toString());
-			td.text(String.valueOf(rowValue.getLosses()));
-			td.endTD();
-
-			// Goals pro
-			td = row.startTD();
-			td.className(cellStyles);
-			td.text(String.valueOf(rowValue.getGoalsPro()));
-			td.endTD();
-
-			// Goals con
-			td = row.startTD();
-			td.className(evenCellStyles.toString());
-			td.text(String.valueOf(rowValue.getGoalsCon()));
-			td.endTD();
-
-			// Goals difference
-			td = row.startTD();
-			td.className(cellStyles);
-			td.text(String.valueOf(rowValue.getGoalsDiff()));
-			td.endTD();
-
-			// Win rate
-			td = row.startTD();
-			td.className(evenCellStyles.toString());
-			td.text(numberFormatter.format(rowValue.getWinRate()));
-			td.endTD();
-
-			row.endTR();
-		}
-	}
-
 	@UiField(provided = false)
 	protected HorizontalPanel panel;
+	
+	@UiField(provided = false)
+	protected VerticalPanel leftPanel;
+	
+	@UiField(provided = false)
+	protected VerticalPanel rightPanel;
 
-	/**
-	 * The main CellTable.
-	 */
-	@UiField(provided = true)
-	protected CellTable<Classification> cellTable;
-
-	/**
-	 * The resources used by this example.
-	 */
-	private Resources resources;
+	private ClassificationTable classification;
 
 	/**
 	 * Construtor padrão.
 	 */
 	public ClassificationViewport() {
 
-		resources = GWT.create(Resources.class);
-		resources.styles().ensureInjected();
+		classification = new ClassificationTable();
 
-		// Create a CellTable.
-
-		// Set a key provider that provides a unique key for each contact. If key is
-		// used to identify contacts when fields (such as the name and address)
-		// change.
-		cellTable = new CellTable<Classification>(Classification.KEY_PROVIDER);
-//		cellTable.setWidth("100%", true);
-//		cellTable.setTitle("Tabela" );
-
-		// Specify a custom table.
-		cellTable.setTableBuilder(new CustomTableBuilder(cellTable));
-
-		// Initialize the columns.
-		initTableColumns();
-
+		
 		// Create the UiBinder.
 		uiBinder.createAndBindUi(this);
-	}
 
-	/**
-	 * Add the columns to the table.
-	 */
-	private void initTableColumns() {
-
-		// Player name
-		Column<Classification, String> firstNameColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return object.getPlayerName();
-			}
-		};
-
-		cellTable.addColumn(firstNameColumn, "Classificação");
-		cellTable.setColumnWidth(firstNameColumn, 240, Unit.PX);
-
-		// Points
-		Column<Classification, String> pointsColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getPoints());
-			}
-		};
-		cellTable.addColumn(pointsColumn, "P");
-		cellTable.setColumnWidth(pointsColumn, 60, Unit.PX);
-
-		// Games
-		Column<Classification, String> gamesColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getNumberOfGames());
-			}
-		};
-
-		cellTable.addColumn(gamesColumn, "J");
-		cellTable.setColumnWidth(gamesColumn, 60, Unit.PX);
-
-		// Wins
-		Column<Classification, String> winsColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getWins());
-			}
-		};
-
-		cellTable.addColumn(winsColumn, "V");
-		cellTable.setColumnWidth(winsColumn, 60, Unit.PX);
-
-		// Ties
-		Column<Classification, String> tiesColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getTies());
-			}
-		};
-
-		cellTable.addColumn(tiesColumn, "E");
-		cellTable.setColumnWidth(tiesColumn, 60, Unit.PX);
-
-		// Losses
-		Column<Classification, String> lossesColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getLosses());
-			}
-		};
-
-		cellTable.addColumn(lossesColumn, "D");
-		cellTable.setColumnWidth(lossesColumn, 60, Unit.PX);
-
-		// Goals pro
-		Column<Classification, String> goalsProColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getGoalsPro());
-			}
-		};
-
-		cellTable.addColumn(goalsProColumn, "GP");
-		cellTable.setColumnWidth(goalsProColumn, 60, Unit.PX);
-
-		// Goals con
-		Column<Classification, String> goalsConColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getGoalsCon());
-			}
-		};
-
-		cellTable.addColumn(goalsConColumn, "GC");
-		cellTable.setColumnWidth(goalsConColumn, 60, Unit.PX);
-
-		// Goals difference
-		Column<Classification, String> goalsDifferenceColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getGoalsDiff());
-			}
-		};
-
-		cellTable.addColumn(goalsDifferenceColumn, "SD");
-		cellTable.setColumnWidth(goalsDifferenceColumn, 60, Unit.PX);
-
-		// Win rate
-		Column<Classification, String> winRateColumn = new Column<Classification, String>(new TextCell()) {
-			@Override
-			public String getValue(Classification object) {
-				return String.valueOf(object.getWinRate());
-			}
-		};
-
-		cellTable.addColumn(winRateColumn, "%");
-		cellTable.setColumnWidth(winRateColumn, 60, Unit.PX);
+		leftPanel.add(classification.asWidget());
+		
 	}
 
 	@Override
@@ -316,10 +52,6 @@ public class ClassificationViewport implements ViewportInterface {
 	}
 
 	public void updateClassification(JsArray<Classification> classifications) {
-		List<Classification> data = new ArrayList<Classification>();
-		for (int i = 0; i < classifications.length(); i++) {
-			data.add(classifications.get(i));
-		}
-		cellTable.setRowData(data);
+		classification.updateClassification(classifications);
 	}
 }
