d6295232966a2b2570c345cec856bf989c9fe0f3
Renato Nunes <renato.nunes@southsystem.com.br>
2018-10-26 18:44:28 -0300

    Dados mockados de exemplo.

--- Back-End/php/modules/base/services/ServicePlayer.php
+++ Back-End/php/modules/base/services/ServicePlayer.php
@@ -53,9 +53,35 @@ class ServicePlayer
       $player = $this->provider_->getPlayer( $id );
       if ( $player != null )
       {
-         $result[] = new DtoReviewInfo( "Nome", $player->getPlayerVO()->name );
-         $result[] = new DtoReviewInfo( "Identificador", $player->getPlayerVO()->id );
-         $result[] = new DtoReviewInfo( "Algo", "Alguma coisa" );
+         $result[] = new DtoReviewInfo( "Posição no Ranking", "2º" );
+         $result[] = new DtoReviewInfo( "Campeonatos Vencidos", "2" );
+         $result[] = new DtoReviewInfo( "Melhor Posição", "1º" );
+         $result[] = new DtoReviewInfo( "Melhor Temporada", "2018" );
+         $result[] = new DtoReviewInfo( "Maior Invencibilidade", "15 jogos" );
+         $result[] = new DtoReviewInfo( "Maior Goleada", "6 x 1" );
+         $result[] = new DtoReviewInfo( "Pior Derrota", "2 x 4" );
+         $result[] = new DtoReviewInfo( "Placar Frequente", "0 x 0" );
+         $result[] = new DtoReviewInfo( "Freguês", "Jogador A" );
+         $result[] = new DtoReviewInfo( "Carrasco", "Jogador H" );
+      }
+      
+      return $result;
+   }
+   
+   public function getStatisticsInfo( int $id ): array
+   {
+      $result = array();
+      
+      $player = $this->provider_->getPlayer( $id );
+      if ( $player != null )
+      {
+         $result[] = new DtoReviewInfo( "Vitórias", "54" );
+         $result[] = new DtoReviewInfo( "Empates", "49" );
+         $result[] = new DtoReviewInfo( "Derrotas", "45" );
+         $result[] = new DtoReviewInfo( "Aproveitamento", "47%" );
+         $result[] = new DtoReviewInfo( "Gols Pró", "70" );
+         $result[] = new DtoReviewInfo( "Gols Contra", "65" );
+         $result[] = new DtoReviewInfo( "Saldo de gols", "5" );
       }
       
       return $result;
