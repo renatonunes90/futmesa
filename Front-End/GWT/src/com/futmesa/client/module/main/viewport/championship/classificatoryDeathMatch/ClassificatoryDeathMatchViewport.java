7b4a1b52b334cc9cc9c06cc791bb8f43c8ba7e2a
Renato Nunes <renato.nunes.stefanini@agcocorp.com>
2019-10-30 15:12:03 -0300

    Created template viewports for every type of championships.

---
+++ Front-End/GWT/src/com/futmesa/client/module/main/viewport/championship/classificatoryDeathMatch/ClassificatoryDeathMatchViewport.java
@@ -0,0 +1,89 @@
+package com.futmesa.client.module.main.viewport.championship.classificatoryDeathMatch;
+
+import com.futmesa.client.businessinteligence.Classification;
+import com.futmesa.client.businessinteligence.Round;
+import com.futmesa.client.module.main.dialogs.results.ResultsDialog;
+import com.futmesa.client.module.main.viewport.championship.ChampionshipViewport;
+import com.futmesa.client.module.main.widgets.classification.ClassificationTable;
+import com.futmesa.client.module.main.widgets.games.GamesTable;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Viewport to championship free for all.
+ */
+public class ClassificatoryDeathMatchViewport extends ChampionshipViewport {
+
+	private static final ClassificatoryDeathMatchUiBinder uiBinder = GWT.create(ClassificatoryDeathMatchUiBinder.class);
+
+	interface ClassificatoryDeathMatchUiBinder extends UiBinder<FluidContainer, ClassificatoryDeathMatchViewport> {
+	}
+
+	@UiField(provided = false)
+	protected FluidContainer panel;
+
+	@UiField(provided = false)
+	protected Column leftPanel;
+
+	@UiField(provided = false)
+	protected Column rightPanel;
+
+	@UiField(provided = false)
+	protected SimplePanel gameTablePanel;
+
+	// @UiField(provided = false)
+	// protected Button insertResultBtn;
+
+	private ClassificationTable classification;
+
+	private GamesTable games;
+
+	private ResultsDialog resultsDialog;
+
+	public ClassificatoryDeathMatchViewport() {
+
+		classification = new ClassificationTable();
+		games = new GamesTable();
+		resultsDialog = new ResultsDialog();
+
+		// Create the UiBinder.
+		uiBinder.createAndBindUi(this);
+
+		leftPanel.add(classification.asWidget());
+		gameTablePanel.add(games.asWidget());
+
+		// rightPanel.setCellHorizontalAlignment( insertResultBtn,
+		// HasHorizontalAlignment.ALIGN_RIGHT);
+		// panel.setCellHorizontalAlignment( championshipLabel,
+		// HasHorizontalAlignment.ALIGN_CENTER );
+
+		/*
+		 * insertResultBtn.addClickHandler( handler ->{ resultsDialog.setChampionship(
+		 * championship ); resultsDialog.setGames( games.getDisplayedGames() );
+		 * resultsDialog.getDialog().center(); resultsDialog.getDialog(); })
+		 */;
+	}
+
+	@Override
+	public Widget asWidget() {
+		return panel;
+	}
+
+	@Override
+	public void updateClassification(JsArray<Classification> classification) {
+		this.classification.updateClassification(classification);
+	}
+
+	@Override
+	public void updateRounds(JsArray<Round> rounds) {
+		games.setRounds(rounds, 21);
+	}
+
+}
---
+++ Front-End/GWT/src/com/futmesa/client/module/main/viewport/championship/classificatoryGroups/ClassificatoryGroupsViewport.java
@@ -0,0 +1,91 @@
+package com.futmesa.client.module.main.viewport.championship.classificatoryGroups;
+
+import com.futmesa.client.base.ViewportInterface;
+import com.futmesa.client.businessinteligence.Championship;
+import com.futmesa.client.businessinteligence.Classification;
+import com.futmesa.client.businessinteligence.Round;
+import com.futmesa.client.module.main.dialogs.results.ResultsDialog;
+import com.futmesa.client.module.main.viewport.championship.ChampionshipViewport;
+import com.futmesa.client.module.main.widgets.classification.ClassificationTable;
+import com.futmesa.client.module.main.widgets.games.GamesTable;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Viewport to championship free for all.
+ */
+public class ClassificatoryGroupsViewport extends ChampionshipViewport {
+
+	private static final ClassificationViewportUiBinder uiBinder = GWT.create(ClassificationViewportUiBinder.class);
+
+	interface ClassificationViewportUiBinder extends UiBinder<HorizontalPanel, ClassificatoryGroupsViewport> {
+	}
+
+	@UiField(provided = false)
+	protected HorizontalPanel panel;
+	
+	@UiField(provided = false)
+	protected VerticalPanel leftPanel;
+
+	@UiField(provided = false)
+	protected VerticalPanel rightPanel;
+	
+	@UiField(provided = false)
+	protected SimplePanel gameTablePanel;
+
+	//@UiField(provided = false)
+	//protected Button insertResultBtn;
+	
+	private ClassificationTable classification;
+
+	private GamesTable games;
+
+	private ResultsDialog resultsDialog;
+	
+	public ClassificatoryGroupsViewport() {
+
+		classification = new ClassificationTable();
+		games = new GamesTable();
+		resultsDialog = new ResultsDialog();
+
+		// Create the UiBinder.
+		uiBinder.createAndBindUi(this);
+
+		leftPanel.add(classification.asWidget());
+		gameTablePanel.add(games.asWidget());
+		
+		// rightPanel.setCellHorizontalAlignment( insertResultBtn, HasHorizontalAlignment.ALIGN_RIGHT);
+		//panel.setCellHorizontalAlignment( championshipLabel, HasHorizontalAlignment.ALIGN_CENTER );
+		
+		/*insertResultBtn.addClickHandler( handler ->{
+			resultsDialog.setChampionship( championship );
+			resultsDialog.setGames( games.getDisplayedGames() );
+			resultsDialog.getDialog().center();
+			resultsDialog.getDialog();
+		})*/;
+	}
+
+	@Override
+	public Widget asWidget() {
+		return panel;
+	}
+
+	@Override
+	public void updateClassification(JsArray<Classification> classification) {
+		this.classification.updateClassification(classification);
+	}
+
+	@Override
+	public void updateRounds(JsArray<Round> rounds) {
+		games.setRounds(rounds, 21);
+	}
+
+}
