92a8b77f0d1f25a2ec936f4d642af83bfd2b1e9c
Renato Nunes <renato.nunes.stefanini@agcocorp.com>
2020-02-06 00:59:23 -0300

    Improvments on deatchmatch phase widget.

--- Front-End/GWT/src/com/futmesa/client/businessinteligence/Game.java
+++ Front-End/GWT/src/com/futmesa/client/businessinteligence/Game.java
@@ -6,103 +6,101 @@ import com.google.gwt.view.client.ProvidesKey;
 /**
  * Classe para representar um jogo de um campeonato retornado pelo back-end.
  */
-public final class Game extends JavaScriptObject
-{
-   
-   /**
-    * Construtor que não será chamado.
-    */
-   protected Game()
-   {
-      // Não faz nada mesmo.
-   }
+public final class Game extends JavaScriptObject {
 
-   /**
-    * The key provider that provides the unique ID of a contact.
-    */
-   public static final ProvidesKey<Game> KEY_PROVIDER = new ProvidesKey<Game>() {
-     @Override
-     public Object getKey(Game item) {
-       return item == null ? null : item.getId();
-     }
-   };
-   
-   /**
-    * @return Retorna o identificador do jogo.
-    */
-   public final native int getId() /*-{
-      return this.id;
-   }-*/;
+	/**
+	 * Construtor que não será chamado.
+	 */
+	protected Game() {
+		// Não faz nada mesmo.
+	}
 
-   /**
-    * @return Retorna o identificador da rodada do campeonato.
-    */
-   public final native int getIdRound() /*-{
-      return this.idround;
-   }-*/;
+	/**
+	 * The key provider that provides the unique ID of a contact.
+	 */
+	public static final ProvidesKey<Game> KEY_PROVIDER = new ProvidesKey<Game>() {
+		@Override
+		public Object getKey(Game item) {
+			return item == null ? null : item.getId();
+		}
+	};
 
-   /**
-    * @return Retorna o identificador do jogador 1.
-    */
-   public final native int getPlayer1Id() /*-{
-      return this.player1.id;
-   }-*/;
+	/**
+	 * @return Retorna o identificador do jogo.
+	 */
+	public final native int getId() /*-{
+		return this.id;
+	}-*/;
 
-   /**
-    * @return Retorna o nome do jogador 1.
-    */
-   public final native String getPlayer1Name() /*-{
-      return this.player1.name;
-   }-*/;
-   
-   /**
-    * @return Retorna o identificador do jogador 2.
-    */
-   public final native int getPlayer2Id() /*-{
-      return this.player2.id;
-   }-*/;
+	/**
+	 * @return Retorna o identificador da rodada do campeonato.
+	 */
+	public final native int getIdRound() /*-{
+		return this.idround;
+	}-*/;
 
-   /**
-    * @return Retorna o nome do jogador 2.
-    */
-   public final native String getPlayer2Name() /*-{
-      return this.player2.name;
-   }-*/;
-   
-  /**
-   * @return Mesa do jogo.
-   */
-  public final native int getGameTable() /*-{
-  	 return this.gametable;
-  }-*/;
+	/**
+	 * @return Retorna o identificador do jogador 1.
+	 */
+	public final native int getPlayer1Id() /*-{
+		return this.player1.id;
+	}-*/;
 
-  /**
-   * @return Gols do jogador 1 no jogo.
-   */
-  public final native Integer getScore1() /*-{
-  	 return this.score1;
-  }-*/;
+	/**
+	 * @return Retorna o nome do jogador 1.
+	 */
+	public final native String getPlayer1Name() /*-{
+		return this.player1.name;
+	}-*/;
 
-  /**
-   * @return Gols do jogador 2 no jogo.
-   */
-  public final native Integer getScore2() /*-{
-  	 return this.score2;
-  }-*/;
+	/**
+	 * @return Retorna o identificador do jogador 2.
+	 */
+	public final native int getPlayer2Id() /*-{
+		return this.player2.id;
+	}-*/;
 
-  /**
-   * @return Horário que o resultado foi inserido.
-   */
-  public final native String getInputDate() /*-{
-  	 return this.inputdate;
-  }-*/;
-  
-  /**
-   * @return Identificador do jogador vencedor.
-   */
-  public final native String getIdWinner() /*-{
-  	 return this.idwinner;
-  }-*/;
+	/**
+	 * @return Retorna o nome do jogador 2.
+	 */
+	public final native String getPlayer2Name() /*-{
+		return this.player2.name;
+	}-*/;
+
+	/**
+	 * @return Mesa do jogo.
+	 */
+	public final native int getGameTable() /*-{
+		return this.gametable;
+	}-*/;
+
+	/**
+	 * @return Gols do jogador 1 no jogo.
+	 */
+	public final native int getScore1() /*-{
+		return this.score1;
+	}-*/;
+
+	/**
+	 * @return Gols do jogador 2 no jogo.
+	 */
+	public final native int getScore2() /*-{
+		return this.score2;
+	}-*/;
+
+	/**
+	 * @return Horário que o resultado foi inserido.
+	 */
+	public final native String getInputDate() /*-{
+		return this.inputdate;
+	}-*/;
+
+	/**
+	 * @return Identificador do jogador vencedor.
+	 */
+	public final native String getIdWinner() /*-{
+		return this.idwinner;
+	}-*/;
 
 	public final native void setScore1(int score) /*-{
 		this.score1 = score;
@@ -113,18 +111,25 @@ public final class Game extends JavaScriptObject
 	}-*/;
 
 	public final native void setPlayer1Name(String name) /*-{
-		  if (!this.player1) {
-		  	this.player1 = {};
-		  }
-	      this.player1.name = name;
-	   }-*/;
+		if (!this.player1) {
+			this.player1 = {};
+		}
+		this.player1.name = name;
+	}-*/;
 
 	public final native void setPlayer2Name(String name) /*-{
 		if (!this.player2) {
-		  	this.player2 = {};
-		  }
-	      this.player2.name = name;
-	   }-*/;
+			this.player2 = {};
+		}
+		this.player2.name = name;
+	}-*/;
+
+	public final native boolean player1IsWinner() /*-{
+		return this.score1 > this.score2;
+	}-*/;
+	
+	public final native boolean hasScores() /*-{
+		return this.score1 !== null && this.score2 != null;
+	}-*/;
 
 }
-
